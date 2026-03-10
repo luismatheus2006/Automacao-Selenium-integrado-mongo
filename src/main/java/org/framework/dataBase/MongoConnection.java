a217b802f6150819a57775a36a4fbf1c29fdff9b
luismatheus2006 <luizmatheusvascon@gmail.com>
2026-03-10 10:27:01 -0300

    CT: retirar o que tava usando em teste local

--- src/main/java/org/framework/dataBase/MongoConnection.java
+++ src/main/java/org/framework/dataBase/MongoConnection.java
@@ -6,10 +6,8 @@ import com.mongodb.client.MongoDatabase;
 
 public class MongoConnection {
 
-   // private static final String URI = System.getenv("MONGO_URI");
-    //private static final String DB = System.getenv("MONGO_DB");
- private static final String URI = "mongodb://expertostech:%40mongo123@localhost:27017";
- private static final String DB = "MONGO_DB";
+   private static final String URI = System.getenv("MONGO_URI");
+   private static final String DB = System.getenv("MONGO_DB");
     private static MongoClient client;
 
     public static MongoDatabase getDatabase(){
