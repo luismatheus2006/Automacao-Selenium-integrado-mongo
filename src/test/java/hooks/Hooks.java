d57e85684df35af8ff1097526146c71a89bfe67f
luismatheus2006 <luizmatheusvascon@gmail.com>
2026-03-11 02:27:02 -0300

    CI: Ajustando relatorios automatizados

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -6,18 +6,38 @@ import org.framework.driver.DriverFactory;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
 public class Hooks {
+
+
 
     @After
     public void tearDown(Scenario scenario) {
+
         try {
+
             WebDriver driver = DriverFactory.getDriver();
 
-            if (driver != null && scenario.isFailed()) {
-                byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-                scenario.attach(screenshot, "image/png", "erro");
+            if (driver != null) {
+
+                byte[] screenshot = ((TakesScreenshot) driver)
+                        .getScreenshotAs(OutputType.BYTES);
+
+                String status = scenario.isFailed() ? "erro" : "sucesso";
+
+                scenario.attach(screenshot, "image/png", status);
+
+                Path path = Path.of(
+                        "target/screenshots/" +
+                                scenario.getName().replaceAll(" ", "_") +
+                                "_" + status + ".png"
+                );
+
+                Files.createDirectories(path.getParent());
+                Files.write(path, screenshot);
             }
+
         } catch (Exception ignored) {
         } finally {
             DriverFactory.quit();
